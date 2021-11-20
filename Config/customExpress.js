1c9fe10cdd42ff2af686694352e7bf4af78ed609
WesleyBueno <wesley.bueno.silva.wbs@gmail.com>
2021-11-20 13:32:26 -0300

    API config

--- Config/customExpress.js
+++ Config/customExpress.js
@@ -1,9 +1,13 @@
 const express = require('express') //importa o express
 const consign = require('consign') //importa o consign
+const bodyParser = require('body-parser') //importa o body-parser
 
 
 module.exports = () => {
     const app = express() //atribui o express para executar o servidor
+
+    app.use(bodyParser.urlencoded({extended: true})) //inclui a biblioteca bodyparser a ser usada no programa
+    app.use(bodyParser.json())
 
     consign()   //inicializa o consign adicionando a pasta de controllers dentro do app
         .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,10 @@
 
-
 module.exports = app => {
     app.get('/atendimento', (req,res) => res.send('Você está na rota de atendimentos e fazendo um get'))
+
+    app.post('/atendimento', (req,res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos e fazendo um post')
+    
+    })
 }
